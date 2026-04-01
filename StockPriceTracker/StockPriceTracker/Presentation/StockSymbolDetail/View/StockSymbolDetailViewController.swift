a4ad1f813edc65e3ad4c33c06f48afa5e22f3ecb
hashim5197288 <129481206+hashim5197288@users.noreply.github.com>
2026-04-01 17:24:46 +0400

    Bootstrap initial project structure
    - Initialized Xcode project (UIKit-based).
    - Set up basic project architecture with MVVM pattern.

---
+++ StockPriceTracker/StockPriceTracker/Presentation/StockSymbolDetail/View/StockSymbolDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StockSymbolDetailViewController.swift
+//  StockPriceTracker
+//
+//  Created by M.Hashim on 01/04/2026.
+//
+
+import UIKit
+
+class StockSymbolDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ StockPriceTracker/StockPriceTracker/Presentation/StockSymbolsList/View/StockSymbolsListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StockSymbolsListViewController.swift
+//  StockPriceTracker
+//
+//  Created by M.Hashim on 01/04/2026.
+//
+
+import UIKit
+
+class StockSymbolsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
